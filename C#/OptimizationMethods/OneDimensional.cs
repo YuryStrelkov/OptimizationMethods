a315dd1ceef3d6381a4fd670b89409fb70bc4358
YuryStrelkov <ghost_strelkov@mail.ru>
2024-10-10 15:59:09 +0400

    Update OneDimensional.cs

--- C#/OptimizationMethods/OneDimensional.cs
+++ C#/OptimizationMethods/OneDimensional.cs
@@ -58,7 +58,7 @@ namespace OptimizationMethods
                 }
             }
 #if DEBUG
-            Console.WriteLine($"GoldenRatio iterations number : {cntr}");
+            Console.WriteLine($"GoldenRatio iterations number : {iteration + 2}");
             Console.WriteLine($"GoldenRatio argument range    : {rhs - lhs}");
 #endif
             return (rhs + lhs) * 0.5;
@@ -107,7 +107,7 @@ namespace OptimizationMethods
                 }
             }
 #if DEBUG
-            Console.WriteLine($"Fibonacci iterations number : {cntr}");
+            Console.WriteLine($"Fibonacci iterations number : {iterations + 2}");
             Console.WriteLine($"Fibonacci argument range    : {rhs - lhs}");
 #endif
             return (rhs + lhs) * 0.5;
